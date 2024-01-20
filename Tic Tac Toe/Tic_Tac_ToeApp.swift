0ff661150dd3a2245fd3f0c7b4737086ffd6212c
Roman Vasyltsov <rgamer402@gmail.com>
2024-01-20 18:40:18 +0200

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Roman Vasyltsov on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
